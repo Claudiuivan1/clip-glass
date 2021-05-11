a4d3f5aee2214c2fe43d0fb0262f5664a2a10509
Claudiu Ivan <claudiu97@hotmail.com>
2021-05-11 10:01:24 +0200

    LRELU 0.4

--- stylegan2/models.py
+++ stylegan2/models.py
@@ -506,7 +506,7 @@ class _BaseParameterizedModel(_BaseModel):
     def __init__(self, **kwargs):
         super(_BaseParameterizedModel, self).__init__()
         self._update_default_kwargs(
-             activation=config.act-fun, #'sigmoid',  #'lrelu:0.2',
+             activation='lrelu:0.4', #config.act-fun, #'sigmoid', 
              lr_mul=1,
              weight_scale=True,
              eps=1e-8
